9bfe43dafeb39bb448f48c01611553168b7f72b2
lcyyadj <486714736@qq.com>
2024-02-29 21:52:37 +0800

    first

---
+++ src/main/java/com/dj/springbootinit/mapper/UserMapper.java
@@ -0,0 +1,16 @@
+package com.dj.springbootinit.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.dj.springbootinit.model.entity.User;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * 用户数据库操作
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
